e79ae8f183a3fb81875403b21939c15faa2879b2
Yorran Petrick <yorranpetrick27@gmail.com>
2024-06-01 21:35:54 -0300

    Testando os Botões

--- app/src/main/java/com/yorran/movieapp/MainActivity.kt
+++ app/src/main/java/com/yorran/movieapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yorran.movieapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,9 +14,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         mainBiding = ActivityMainBinding.inflate(layoutInflater)
-        ViewCompat.setOnApplyWindowInsetsListener(mainBiding.root) { v, insets ->
+        setContentView(mainBiding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -23,6 +24,11 @@ class MainActivity : AppCompatActivity() {
 
         mainBiding.loginBtn.setOnClickListener{
             startActivity(Intent(this, NewAccountActivity::class.java))
+            Toast.makeText(this, "BTN clicado", Toast.LENGTH_LONG).show()
+        }
+
+        mainBiding.nwwAccontBtn.setOnClickListener{
+            Toast.makeText(this, "BTN clicado", Toast.LENGTH_LONG).show()
         }
     }
 }
