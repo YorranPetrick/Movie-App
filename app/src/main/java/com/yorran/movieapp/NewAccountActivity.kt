65ff26cbcda344c7afca24d723a83338154ff237
Yorran Petrick <yorranpetrick27@gmail.com>
2024-06-01 21:24:59 -0300

    Criando a Activity New Account

--- app/src/main/java/com/yorran/movieapp/NewAccountActivity.kt
+++ app/src/main/java/com/yorran/movieapp/NewAccountActivity.kt
@@ -1,28 +1,20 @@
 package com.yorran.movieapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.yorran.movieapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var mainBiding: ActivityMainBinding
+class NewAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        mainBiding = ActivityMainBinding.inflate(layoutInflater)
-        ViewCompat.setOnApplyWindowInsetsListener(mainBiding.root) { v, insets ->
+        setContentView(R.layout.activity_new_account)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        mainBiding.loginBtn.setOnClickListener{
-            startActivity(Intent(this, NewAccountActivity::class.java))
         }
     }
 }
