fe4f7623e0b3e6506ab235d9f60268feee6d2eda
Yorran Petrick <yorranpetrick27@gmail.com>
2024-06-07 20:25:30 -0300

    Estilizando a Form Login

--- app/src/main/java/com/yorran/movieapp/FormLoginActivity.kt
+++ app/src/main/java/com/yorran/movieapp/FormLoginActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.yorran.movieapp.databinding.ActivityFormLoginBinding
 
 class FormLoginActivity : AppCompatActivity() {
+
+    private lateinit var formBiding : ActivityFormLoginBinding
+    private lateinit var googleSignInClient : GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_form_login)
+        formBiding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(formBiding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        formBiding.backBtn.setOnClickListener{
+           finish()
         }
     }
 }
