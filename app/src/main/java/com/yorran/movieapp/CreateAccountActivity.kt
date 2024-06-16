6a12a373456f87156c15bc63a5614d619394be2c
Yorran Petrick <yorranpetrick27@gmail.com>
2024-06-16 11:07:05 -0300

    Criando o Layout de cadastro de novas contas

--- app/src/main/java/com/yorran/movieapp/CreateAccountActivity.kt
+++ app/src/main/java/com/yorran/movieapp/CreateAccountActivity.kt
@@ -1,33 +1,23 @@
 package com.yorran.movieapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.yorran.movieapp.databinding.ActivityNewAccountBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
-class NewAccountActivity : AppCompatActivity() {
-    private lateinit var accountBinding: ActivityNewAccountBinding
+class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        accountBinding = ActivityNewAccountBinding.inflate(layoutInflater)
-        setContentView(accountBinding.root)
+        setContentView(R.layout.activity_create_account)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        accountBinding.backBtn.setOnClickListener{
-            finish()
-        }
 
-        accountBinding.nextBtn.setOnClickListener{
-            startActivity(Intent(this, CreateAccountActivity::class.java))
-
-        }
     }
 }
